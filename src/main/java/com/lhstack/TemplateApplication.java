bc83343f39d981ab7f4548786e72a7de17a1818a
632d6423b4d9145d677b07db <632d6423b4d9145d677b07db@devide>
2023-04-21 09:52:23 +0000

    UPDATE

---
+++ src/main/java/com/lhstack/TemplateApplication.java
@@ -0,0 +1,10 @@
+package com.lhstack;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
