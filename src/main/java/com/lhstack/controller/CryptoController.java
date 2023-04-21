e8293a77e46293dce911cf373934b02df3fa646d
632d6423b4d9145d677b07db <632d6423b4d9145d677b07db@devide>
2023-04-21 10:29:22 +0000

    Auto commit

--- src/main/java/com/lhstack/controller/CryptoController.java
+++ src/main/java/com/lhstack/controller/CryptoController.java
@@ -1,24 +1,39 @@
 package com.lhstack.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lhstack.utils.Aes;
+
 import io.reactivex.Single;
 
 @RestController
 @RequestMapping("crypto")
 public class CryptoController {
+
+    @Value("${aes.key:}")
+    private String defaultAesKey;
+
+    @Value("${aes.iv:}")
+    private String defaultAesIv;
     
     @PostMapping("decrypt")
     public Single<String> decrypt(@RequestBody Map<String,String> body){
         return Single.defer(() -> {
-            System.out.println(body);
-            return Single.just("hello world");
+            String aesKey = body.getOrDefault("key", defaultAesKey);
+            String aesIv = body.getOrDefault("iv", defaultAesIv);
+            byte[] content = body.getOrDefault("content","").getBytes();
+            return Single.fromCallable(() -> {
+                Optional<byte[]> resulOptional = Aes.decrypt(aesKey, aesIv, content);
+                return "hello world";
+            });
         });
     }
 
